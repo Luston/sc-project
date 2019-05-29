bef4023f436a1b679465194d25645252d567e62f
lean <601857337@qq.com>
2019-05-29 15:27:05 +0800

    hystrix dashboard配置，首次提交

--- sc-client/src/main/java/com/springcloud/scclient/ScClientApplication.java
+++ sc-client/src/main/java/com/springcloud/scclient/ScClientApplication.java
@@ -1,12 +1,16 @@
 package com.springcloud.scclient;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+
 @EnableHystrix
 @SpringCloudApplication
 public class ScClientApplication {
@@ -14,4 +18,13 @@ public class ScClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScClientApplication.class, args);
 	}
+	/*@Bean
+	public ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}*/
 }
