0634b5aa62b1c8723e5fca6187aca7c849ed1d71
lean <601857337@qq.com>
2019-06-20 16:37:44 +0800

    配置消息总线，实现动态刷新配置文件

--- sc-zuul/src/main/java/com/springcloud/ScZuulApplication.java
+++ sc-zuul/src/main/java/com/springcloud/ScZuulApplication.java
@@ -2,10 +2,12 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringCloudApplication
+@RefreshScope
 public class ScZuulApplication {
 
 	public static void main(String[] args) {
